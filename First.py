8d6d61323e5fbe807752a440fa2c704ce5dfeee5
Vaibhav <vaibhavgothwal123@gmail.com>
2025-01-09 20:34:22 +0530

    Updated design

---
+++ First.py
@@ -0,0 +1,19 @@
+# Write a program to check if a list is a palindrome or not
+# list=["Civic","Madam","1"]
+# list2=(list.copy())
+# list2.reverse()
+# print(list)
+# print(list2)
+# if(list==list2):
+#     print("The list is a palindrome list.")
+# else:
+#     print("The list is not a palindrome list.")
+
+# write a program to count the number of A's and sort it in the following tuple
+# tup=("C","D","A","A","B","A")
+# print(tup.count("A"))
+
+
+# list=["C","D","A","A","B","A"]
+# list.sort()
+# print(list)
